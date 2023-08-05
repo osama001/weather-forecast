21119310d5d5142b152f99ca83eeae847bc42812
osama001 <osamasherif001@gmail.com>
2023-08-06 02:19:29 +0300

    replace  magic numbers

--- js/main.js
+++ js/main.js
@@ -38,34 +38,35 @@ let days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'
 async function getWeather(city){
     let dataReq = await fetch(` http://api.weatherapi.com/v1/forecast.json?key=cef5e7e3db69496899700907230508&q=${city}&days=7`)
     let dataRes = await dataReq.json()
-     currentDay= new Date(dataRes.current.last_updated)
-     currentINdex = currentDay.getDay()
-     currentDayName=days[currentINdex]
-     day1.innerHTML=currentDayName
-     date1.innerHTML=currentDay.toString().slice(3,10)
-     if(currentINdex => 6 ){
-        currentINdex = 0
-
-        tomorrow= days[currentINdex]
-        day2.innerHTML=tomorrow
-        after2days= days[currentINdex+1]
-        day3.innerHTML=after2days
-     }else{
-        tomorrow= days[currentINdex+1]
-        day2.innerHTML=tomorrow
-        after2days= days[currentINdex+2]
-        day3.innerHTML=after2days
-     }
-    Firstsrc = dataRes.current.condition.icon
-     FirstText= dataRes.current.condition.text
-     Ftemp.innerHTML=  dataRes.current.temp_c
-     citylab.innerHTML=dataRes.location.name
-     firstDesc.innerHTML=FirstText
-    
+   let  currentDay= new Date(dataRes.current.last_updated)
+   let currentINdex = currentDay.getDay()
+   let currentDayName=days[currentINdex]
+   day1.innerHTML=currentDayName
+  date1.innerHTML=currentDay.toString().slice(3,10)
      
-     day1img.setAttribute('src',Firstsrc)
+       
+        
+   let    Firstsrc = dataRes.current.condition.icon
+     day1img.src=Firstsrc
+   let     FirstText= dataRes.current.condition.text
+             Ftemp.innerHTML=  dataRes.current.temp_c
+             citylab.innerHTML=dataRes.location.name
+             firstDesc.innerHTML=FirstText
 
-     // day2 settings 
+  
+             
+             
+             
+             
+             
+             
+             
+             // day2 settings 
+             
+           
+             let   tomorrow = new Date(dataRes.forecast.forecastday[1].date)
+             let   tomorrowName= days[tomorrow.getDay()]
+             day2.innerHTML=tomorrowName
      
      day2img.src = dataRes.forecast.forecastday[1].day.condition.icon
      day2desc.innerHTML = dataRes.forecast.forecastday[1].day.condition.text
@@ -73,22 +74,22 @@ async function getWeather(city){
      day2temps.innerHTML=dataRes.forecast.forecastday[1].day.mintemp_c
 
         //day3 settings
+        after2days= new Date(dataRes.forecast.forecastday[2].date)
+        day3.innerHTML= days[after2days.getDay()]
      thirdsrc = dataRes.forecast.forecastday[2].day.condition.icon
      
      day3img.setAttribute('src',thirdsrc)
      day3desc.innerHTML = dataRes.forecast.forecastday[2].day.condition.text
      day3temp.innerHTML=dataRes.forecast.forecastday[2].day.maxtemp_c
      day3temps.innerHTML=dataRes.forecast.forecastday[2].day.mintemp_c
-     console.log(day1img,day2img,day3img)
+    
 
 }
 
 
 
 getWeather('egypt')
-  function print(a){
-    console.log(a)
-  }
+
 searchinp.addEventListener('input',function(e){
     getWeather(e.target.value)
 })
